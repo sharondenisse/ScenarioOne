3960fe231e3d13f3451f8061d7bcd02ee1c315ba
Sharon Denisse <sharondenisse@users.noreply.github.com>
2018-08-28 13:14:44 -0400

    Python code upload
    Code to use as collaboration file

---
+++ forTheGIT.py
@@ -0,0 +1,22 @@
+# Sharon D. Kenny
+# Writing a program for git
+# -----------------------------
+# Edit file by deleting active code
+# and using commented lines instead
+
+print("Hello world")
+#print('Hello world!')
+
+def happy():
+	return ("Hello!")
+
+happy()
+
+# Use this code to change the file
+#def happy():
+#	x = 3
+#	y = .141592
+#	pii = x + y
+#	print(pii)
+#
+#happy()
