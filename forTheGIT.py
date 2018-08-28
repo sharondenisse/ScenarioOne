744370842f5ddec56e3bf616aa285024253adcc8
Sharon Denisse <sharondenisse@users.noreply.github.com>
2018-08-28 14:09:37 -0400

    Revert "Changing active function happy()"

--- forTheGIT.py
+++ forTheGIT.py
@@ -7,16 +7,16 @@
 print("Hello world!")
 #print('Hello world!')
 
-#def happy():
-#	return ("Hello!")
-
-#happy()
-
-# Use this code to change the file
 def happy():
-	x = 3
-	y = .141592
-	pii = x + y
-	print(pii)
+	return ("Hello!")
 
 happy()
+
+# Use this code to change the file
+#def happy():
+#	x = 3
+#	y = .141592
+#	pii = x + y
+#	print(pii)
+#
+#happy()
